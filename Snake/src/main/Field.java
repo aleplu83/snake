4f8c60a6e0b3754eb9525ab937b0a7e389606ce2
Alessandro Pluchino <aleplu@itpla.be>
2025-02-22 18:52:03 +0100

    removed unused import

--- Snake/src/main/Field.java
+++ Snake/src/main/Field.java
@@ -5,7 +5,6 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
