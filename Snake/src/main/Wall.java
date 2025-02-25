d2bea4fab65545c483a28792ab267db0fdc62862
Alessandro Pluchino <aleplu@itpla.be>
2025-02-25 09:58:22 +0100

    worked on Wall class

--- Snake/src/main/Wall.java
+++ Snake/src/main/Wall.java
@@ -1,15 +1,18 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Point;
 import java.awt.Rectangle;
 
 public class Wall {
-
-	private Point[] body; 
-	private Rectangle[] walls;
+ 
+	private Rectangle[] blocks;
+	private Color color;
 	
-	public Wall(int startX,int startY,int endX,int endY) {
+	public Wall(Color color,int startX,int startY,int length) {
 		// TODO Auto-generated constructor stub
+		blocks = new Rectangle[length];
+		this.color=color;
 		
 	}
 
