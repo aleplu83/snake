18eb1b857cfdc57296e7181daa7a23a6c5a55322
Alessandro Pluchino <aleplu@itpla.be>
2025-02-25 18:20:07 +0100

    redesigned main window

--- Snake/src/main/Window.java
+++ Snake/src/main/Window.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
@@ -8,35 +7,35 @@ import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
 public class Window {
 
 	private JFrame mainWindow;
-	private JPanel fieldPanel,statsPanel;
+	private JPanel statsPanel;
 	private Field field;
 	private JLabel lblStats;
 	
 	public Window() {
 		mainWindow = new JFrame("Snake");
-		mainWindow.setLocationByPlatform(true);
-		fieldPanel = new JPanel();
-		fieldPanel.setPreferredSize(new Dimension(600,600));
-		field = new Field(new Dimension(600,600));
-		fieldPanel.add(field);
-		statsPanel = new JPanel(new FlowLayout());
+		field = new Field(new Dimension(500,500));
+		statsPanel = new JPanel();
 		lblStats = new JLabel();
+		
+		mainWindow.setSize(800,600);
+		mainWindow.setLocation(400, 200);
+		mainWindow.setLayout(new FlowLayout());
+		
+		
+		
 		lblStats.setBorder(BorderFactory.createTitledBorder("Level"));
-		lblStats.setVerticalTextPosition(SwingConstants.TOP);
-		lblStats.setHorizontalTextPosition(SwingConstants.LEFT);
 		lblStats.setText("Points: ");
-		mainWindow.setLayout(new BorderLayout());
-		mainWindow.add(fieldPanel,BorderLayout.WEST);
-		statsPanel.add(lblStats,FlowLayout.LEFT);
-		mainWindow.add(statsPanel,BorderLayout.SOUTH);
-		mainWindow.setSize(800,700);
+		
+		mainWindow.add(field);
+		statsPanel.add(lblStats);
+		mainWindow.add(statsPanel);
+		
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//mainWindow.pack();
+		mainWindow.pack();
 		mainWindow.setVisible(true);
 		mainWindow.setResizable(false);
 		
