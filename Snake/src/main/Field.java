79732a76b041886a5c80336087a863d13315cab8
Alessandro Pluchino <aleplu@itpla.be>
2025-02-25 18:19:11 +0100

    improved game and points system

--- Snake/src/main/Field.java
+++ Snake/src/main/Field.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -15,10 +16,13 @@ public class Field extends JPanel implements KeyListener,Runnable {
 	private Snake snake;
 	private Thread thread;
 	private Fruit fruit;
+	private int totalPoints=0;
 	private int fruitsTotal=0;
+	private int fruitsMissed=0;
 	private int level=0;
 	private int levels[] = {0,15,30,70,120,250,400};
 	private int speeds[] = {350,300,260,200,150,100,80};
+	private int timeouts[] = {45,40,35,30,25,20,15};
 	private Dimension fieldSize;
 	
 	public Field(Dimension size) {
@@ -31,14 +35,14 @@ public class Field extends JPanel implements KeyListener,Runnable {
 		requestFocus();
 		
 		snake = new Snake();
-		fruit = new Fruit(getFruitPos(),Color.RED,1,-1);
+		fruit = new Fruit(newFruitPos(),Color.RED,speeds[level]/speeds[level]*timeouts[level],speeds[level]/speeds[level]*timeouts[level]);
 		
 		thread = new Thread(this);
 		thread.start();
 		
 	}
 	
-	private Point getFruitPos() {
+	private Point newFruitPos() {
 		Point p = new Point();
 		do {
 			p.x=getRnd(10,fieldSize.width-10);
@@ -81,7 +85,10 @@ public class Field extends JPanel implements KeyListener,Runnable {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		drawSnake(g);
-		drawFruit(g);
+		if (fruit.getTimeout()!=0)
+			drawFruit(g);
+		
+		
 	}
 	
 	/**
@@ -111,14 +118,16 @@ public class Field extends JPanel implements KeyListener,Runnable {
 
 	private void printStats() {
 		System.out.println("Level: "+level);
-		System.out.println("Fruits eaten: "+fruitsTotal);
+		System.out.println("Fruits eaten/missed: "+fruitsTotal+"/"+fruitsMissed);
+		System.out.println("Total points: "+totalPoints);
 	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		while (snake.move()) {
-			
+			/*System.out.println(speeds[level]/speeds[level]*5);
+			System.out.println(fruit.getTimeout());*/
 			repaint();
 			
 			try {
@@ -132,11 +141,21 @@ public class Field extends JPanel implements KeyListener,Runnable {
 				gameOver();
 			}
 			
+			if (fruit.getTimeout()!=0) {
+				fruit.setTimeout(fruit.getTimeout()-1);
+			} else {
+				fruitsMissed+=1;
+				fruit = new Fruit(newFruitPos(),Color.RED,speeds[level]/speeds[level]*timeouts[level],speeds[level]/speeds[level]*timeouts[level]);
+				printStats();
+			}
+				
+			
 			if (gotFruit()) {
 				snake.grow();
 				fruitsTotal+=1;
+				totalPoints+=fruit.getTimeout();
 				
-				fruit.setPos(getFruitPos());
+				fruit = new Fruit(newFruitPos(),Color.RED,speeds[level]/speeds[level]*timeouts[level],speeds[level]/speeds[level]*timeouts[level]);
 				for (int i=0;i<levels.length;i++) {
 					if (fruitsTotal==levels[i]) {
 						level=i; //raise level
@@ -168,8 +187,18 @@ public class Field extends JPanel implements KeyListener,Runnable {
 	
 	public void drawFruit(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g.create();
-		g2d.setColor(Color.RED);
+		g2d.setColor(fruit.getColor());
         g2d.fillOval(fruit.getPos().x,fruit.getPos().y,10,10);
+        if (fruit.getTimeout()>30) {
+        	g2d.setColor(Color.LIGHT_GRAY);
+        } else if (fruit.getTimeout()>15 && fruit.getTimeout()<30) {
+        	g2d.setColor(Color.YELLOW);
+        } else if (fruit.getTimeout()<15) {
+        	g2d.setColor(Color.RED);
+        }
+        	
+        g2d.setFont(new Font("Tahoma", Font.PLAIN, 10));
+        g2d.drawString(fruit.getTimeout()+"", fruit.getPos().x+10, fruit.getPos().y+15);
         g2d.setColor(Color.GREEN);
         g2d.drawLine(fruit.getPos().x+5, fruit.getPos().y, fruit.getPos().x, fruit.getPos().y-3);
         g2d.drawLine(fruit.getPos().x+5, fruit.getPos().y, fruit.getPos().x+10, fruit.getPos().y-3);
