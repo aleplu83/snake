54e12309bb7f5df41b558382f06d84e5c83c9451
Alessandro Pluchino <aleplu@itpla.be>
2025-02-28 13:54:01 +0100

    code corr

--- Snake/src/main/Field.java
+++ Snake/src/main/Field.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
@@ -16,13 +17,13 @@ public class Field extends JPanel implements KeyListener,Runnable {
 	private Snake snake;
 	private Thread thread;
 	private Fruit fruit;
-	private int totalPoints=0;
-	private int fruitsTotal=0;
+	private int points=0;
+	private int fruitsEaten=0;
 	private int fruitsMissed=0;
 	private int level=0;
-	private int levels[] = {0,15,30,70,120,250,400};
+	private int levels[] = {0,50,100,170,220,350,400};
 	private int speeds[] = {350,300,260,200,150,100,80};
-	private int timeouts[] = {45,40,35,30,25,20,15};
+	private Wall[] walls;
 	private Dimension fieldSize;
 	
 	public Field(Dimension size) {
@@ -35,10 +36,15 @@ public class Field extends JPanel implements KeyListener,Runnable {
 		
 		snake = new Snake();
 		fruit = new Fruit(newFruitPos(),Color.RED,1,-1);
+		createWalls();
 		
 		thread = new Thread(this);
 		thread.start();
 		
+	}
+	
+	private void createWalls() {
+		walls = new Wall[2];
 	}
 	
 	private Point newFruitPos() {
@@ -52,13 +58,20 @@ public class Field extends JPanel implements KeyListener,Runnable {
 	
 	private void gameOver() {
 		level=0;
-		fruitsTotal=0;
+		fruitsEaten=0;
+		fruitsMissed=0;
+		
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		snake = new Snake();
+		fruit = new Fruit(newFruitPos(),Color.RED,1,-1);
+		
+		thread = new Thread(this);
+		thread.start();
 	}
 
 	@Override
@@ -117,15 +130,13 @@ public class Field extends JPanel implements KeyListener,Runnable {
 
 	private void printStats() {
 		System.out.println("Level: "+level);
-		System.out.println("Fruits eaten/missed: "+fruitsTotal+"/"+fruitsMissed);
-		System.out.println("Total points: "+totalPoints);
+		System.out.println("Fruits eaten/missed: "+fruitsEaten+"/"+fruitsMissed);
+		System.out.println("Total points: "+points);
 	}
 	
 	@Override
 	public void run() {
 		while (snake.move()) {
-			/*System.out.println(speeds[level]/speeds[level]*5);
-			System.out.println(fruit.getTimeout());*/
 			repaint();
 			
 			try {
@@ -139,22 +150,14 @@ public class Field extends JPanel implements KeyListener,Runnable {
 				gameOver();
 			}
 			
-			/*if (fruit.getTimeout()!=0) {
-				fruit.setTimeout(fruit.getTimeout()-1);
-			} else {
-				fruitsMissed+=1;
-				fruit = new Fruit(newFruitPos(),Color.RED,speeds[level]/speeds[level]*timeouts[level],speeds[level]/speeds[level]*timeouts[level]);
-				printStats();
-			}*/
-			
 			if (gotFruit()) {
 				snake.grow();
-				fruitsTotal+=1;
-				totalPoints+=fruit.getValue();
+				fruitsEaten+=1;
+				points+=fruit.getValue();
 				
 				fruit = new Fruit(newFruitPos(),Color.RED,1,-1);
 				for (int i=0;i<levels.length;i++) {
-					if (fruitsTotal==levels[i]) {
+					if (fruitsEaten==levels[i]) {
 						level=i; //raise level
 					}
 				}
@@ -186,43 +189,18 @@ public class Field extends JPanel implements KeyListener,Runnable {
 		Graphics2D g2d = (Graphics2D) g.create();
 		g2d.setColor(fruit.getColor());
         g2d.fillOval(fruit.getPos().x,fruit.getPos().y,10,10);
-        /*if (fruit.getTimeout()>30) {
-        	g2d.setColor(Color.LIGHT_GRAY);
-        } else if (fruit.getTimeout()>15 && fruit.getTimeout()<30) {
-        	g2d.setColor(Color.YELLOW);
-        } else if (fruit.getTimeout()<15) {
-        	g2d.setColor(Color.RED);
-        }
-        g2d.setFont(new Font("Tahoma", Font.PLAIN, 10));
-        if (fruit.getPos().x+10 > fieldSize.width-10) {
-        	g2d.drawString(fruit.getTimeout()+"", fruit.getPos().x-10, fruit.getPos().y+10);
-        } else if (fruit.getPos().y+20 > fieldSize.height-10) {
-        	g2d.drawString(fruit.getTimeout()+"", fruit.getPos().x+10, fruit.getPos().y-10);
-        } else if ((fruit.getPos().x+20 > fieldSize.width-10) && (fruit.getPos().y+20 > fieldSize.height-10)) {
-        	g2d.drawString(fruit.getTimeout()+"", fruit.getPos().x-10, fruit.getPos().y-10);
-        } else {
-        	g2d.drawString(fruit.getTimeout()+"", fruit.getPos().x+10, fruit.getPos().y+15);
-        }*/
         g2d.setColor(Color.GREEN);
         g2d.drawLine(fruit.getPos().x+5, fruit.getPos().y, fruit.getPos().x, fruit.getPos().y-3);
         g2d.drawLine(fruit.getPos().x+5, fruit.getPos().y, fruit.getPos().x+10, fruit.getPos().y-3);
 		g2d.dispose();
 	}
-
-	public int getFruits() {
-		// TODO Auto-generated method stub
-		return fruitsTotal;
-	}
-
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
--- Snake/src/main/Wall.java
+++ Snake/src/main/Wall.java
@@ -1,7 +1,6 @@
 package main;
 
 import java.awt.Color;
-import java.awt.Point;
 import java.awt.Rectangle;
 
 public class Wall {
@@ -9,12 +8,36 @@ public class Wall {
 	private Rectangle[] blocks;
 	private Color color;
 	
-	public Wall(Color color,int startX,int startY,int length) {
-		// TODO Auto-generated constructor stub
-		blocks = new Rectangle[length];
+	public Wall(Color color,int startX,int startY,int endX,int endY) {
 		this.color=color;
-		
-		
+	}
+
+	/**
+	 * @return the blocks
+	 */
+	public Rectangle[] getBlocks() {
+		return blocks;
+	}
+
+	/**
+	 * @param blocks the blocks to set
+	 */
+	public void setBlocks(Rectangle[] blocks) {
+		this.blocks = blocks;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public Color getColor() {
+		return color;
+	}
+
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(Color color) {
+		this.color = color;
 	}
 	
 	
