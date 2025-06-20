b8bdc628eef1e4134fc10ee3a3c0cb08df703551
Alessandro Pluchino <aleplu@itpla.be>
2025-06-20 14:12:19 +0200

    removed stats panel

--- Snake/src/main/Window.java
+++ Snake/src/main/Window.java
@@ -10,29 +10,18 @@ import javax.swing.JPanel;
 
 public class Window {
 
-	private JFrame mainWindow;
-	private JPanel statsPanel;
+	private final JFrame mainWindow;
 	private Field field;
-	private JLabel lblStats;
 	
 	public Window() {
 		mainWindow = new JFrame("Snake");
 		field = new Field(new Dimension(500,500));
-		statsPanel = new JPanel();
-		lblStats = new JLabel();
 		
 		mainWindow.setSize(800,600);
 		mainWindow.setLocation(400, 200);
 		mainWindow.setLayout(new FlowLayout());
 		
-		
-		
-		lblStats.setBorder(BorderFactory.createTitledBorder("Level"));
-		lblStats.setText("Points: ");
-		
 		mainWindow.add(field);
-		statsPanel.add(lblStats);
-		mainWindow.add(statsPanel);
 		
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainWindow.pack();
